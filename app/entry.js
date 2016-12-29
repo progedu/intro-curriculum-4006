32df011c6988cc16b4475959ca0ff4063f995aeb
yudai <uehara.yudai@gmail.com>
2016-12-29 11:12:24 +0000

    ランダムバイトをHTML上に表示

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,7 @@
 'use strict';
 const dc = require('damage-calc');
+const randomStr = require('crypto').randomBytes(8).toString('hex');
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
   dc.effectiveDamage(100, 50, 30) + '</p>');
+document.writeln('<p>' + randomStr + '</p>');
