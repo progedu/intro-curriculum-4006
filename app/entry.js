a3c02a612ba27bcc231ff71f51b6c8b9019330d9
kuchinashi-gadget <kuchinashi.gadget@gmail.com>
2017-03-22 08:18:42 +0000

    ランダムな文字列が表示されるように修正

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,10 @@
 'use strict';
 const dc = require('damage-calc');
+const cy = require('crypto');
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
   dc.effectiveDamage(100, 50, 30) + '</p>');
+
+document.writeln(
+  '<p>あなたのランダムな文字列は、' +
+  cy.randomBytes(8).toString('hex') + '</p>');
