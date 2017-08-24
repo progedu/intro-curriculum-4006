ccdd2f0e98101a6496d19bcb4ed4e09bee920253
yusuke_nakajima <www.yyworld@gmail.com>
2017-08-25 00:02:42 +0900

    コード整形

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,6 @@
 'use strict';
 const dc = require('damage-calc');
 document.writeln(
-  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+    '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
+    dc.effectiveDamage(100, 50, 30) + '</p>'
+);
