561d1ec190733bd383243747cd7eca68450d2e66
nedew <nero.nh7@gmail.com>
2017-11-01 13:15:19 +0000

    cryptoのテスト

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,10 @@
 'use strict';
 const dc = require('damage-calc');
+
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+  dc.effectiveDamage(100, 50, 30) + ' です。</p>');
+
+document.writeln(
+  '<p>ランダムな8バイトの値の16進数文字列: ' + require('crypto').randomBytes(8).toString('hex') + '</p>'
+);
