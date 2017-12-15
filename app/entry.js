f58cfac7dd9fba685249b7a7aac80f6cef5b3699
youk233_231 <yuuki_16n1100449@nnn.ed.jp>
2017-12-15 04:15:36 +0000

    cryptoモジュール追加導入

--- app/entry.js
+++ app/entry.js
@@ -4,5 +4,5 @@ document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
   dc.effectiveDamage(100, 50, 30) + '</p>');
 
-// document.writeln(
-//   '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
+document.writeln(
+  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
