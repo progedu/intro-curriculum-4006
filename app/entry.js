bc49d379e786962c3012bd13d9b58e4043a7ec6e
elying <eri.tanaka213@gmail.com>
2018-01-11 12:15:18 +0900

    crypto モジュールも動かすように追加

--- app/entry.js
+++ app/entry.js
@@ -2,4 +2,7 @@
 const dc = require('damage-calc');
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+    dc.effectiveDamage(100, 50, 30) + '</p>');
+
+document.writeln(
+  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
