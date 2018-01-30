f672b051c78f16554d33495552580180aa5042e9
acua98dh <vbnsibc@yahoo.co.jp>
2018-01-30 08:27:21 +0000

    Node.jsのcryptoモジュールをwebpackで再現

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
 const dc = require('damage-calc');
+// document.writeln(
+//   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
+//   dc.effectiveDamage(100, 50, 30) + '</p>');
 document.writeln(
-  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+  '<br>ランダムな8バイトの文字列<br>' +
+  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
