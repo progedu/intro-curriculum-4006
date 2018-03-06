78fdb6cf15b4f761548eb0b60d6eaa8557fb62e8
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2018-03-06 07:18:05 +0000

    webpackを利用してcryptoモジュールも使用してみる

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,11 @@
 'use strict';
+// 'damage-calc'モジュールを読み込む
 const dc = require('damage-calc');
+// 'crypto'モジュールを読み込む
+const crypto = require('crypto');
+
 document.writeln(
   '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
   dc.effectiveDamage(100, 50, 30) + '</p>');
+document.writeln(
+  '<p>' + crypto.randomBytes(8).toString('hex') + '</p>');
