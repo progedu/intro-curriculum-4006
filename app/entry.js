417d5140a25cd6df5bbd92b0904fb17cda7390d9
rabosakaki <rabosakaki.scratch@gmail.com>
2018-06-19 02:52:59 +0000

    cryptoモジュールを動かしてHTMLに表示

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,5 @@
 'use strict';
-import dc from 'damage-calc';
+import crypto from 'crypto';
 document.writeln(
-  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+  '<p>' + crypto.randomBytes(8).toString('hex')+ '</p>'
+);
