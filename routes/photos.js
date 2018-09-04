1784f907d66a8a09b5b0d44388802f6ebd6a3551
Shun Nishimura <n.shun1110@gmail.com>
2018-09-04 15:42:49 +0900

    update modules

--- routes/photos.js
+++ routes/photos.js
@@ -1,6 +1,6 @@
 'use strict';
-const  express = require('express');
-const  router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.send('Some photos');
