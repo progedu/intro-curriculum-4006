2ecd16cdf4f92020632f7e29bf86ea0a99ab4837
lo-ngtail <lo0oo0ongtail@gmail.com>
2019-02-07 08:55:11 +0900

    crypto でランダムな文字列を生成して表示

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,4 @@
 'use strict';
-import dc from 'damage-calc';
+const crypto = require('crypto');
 document.writeln(
-  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
+  '<p>' + crypto.randomBytes(8).toString('hex') + '</p>');
