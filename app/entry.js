5dad9844195d4112e41dacb97bdd863ccd26d34b
piyo2019 <hakobe2001@yahoo.co.jp>
2020-01-12 13:04:00 +0900

    表示する文字列の説明を追加（Hatoremiさんのプルリクを参考にしました）

--- app/entry.js
+++ app/entry.js
@@ -3,5 +3,6 @@ import dc from 'damage-calc';
 import crypto from 'crypto';
 const root = document.getElementById('root');
 root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p><p>' 
+  + dc.effectiveDamage(100, 50, 30) + '</p>' 
+  + 'ランダムな8バイト値の16進数の文字列：'
   + crypto.randomBytes(8).toString('hex') + '</p>';
