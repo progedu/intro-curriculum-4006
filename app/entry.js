98dec696f2ea1d8f01ef290d2673748c163cecec
nanachan2019 <nanachan59+github2019@gmail.com>
2020-01-14 17:40:59 +0900

    cryptoモジュールをクライアントで動かす

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,4 @@
 'use strict';
-import dc from 'damage-calc';
+import crypt from 'crypto';
 const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+root.innerHTML = `<p>${crypt.randomBytes(8).toString('hex')}</p>`;
