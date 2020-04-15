74f57b2eb04f78f9056757729c363846bd5da1ee
rocoh <rocoh@LAPTOP-464UG7P3.localdomain>
2020-04-16 06:30:45 +0900

    cryptoの動作確認

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
+import crypto from 'crypto';
+
 import dc from 'damage-calc';
 const root = document.getElementById('root');
 root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+  + dc.effectiveDamage(100, 50, 30) + '</p><p>'
+  + crypto.randomBytes(8).toString('hex') + '</p>';
