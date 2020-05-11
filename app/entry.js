32e3150f6b6470e12d50b520ccd8f9192df1e4d3
Tomohiro Ishikawa <t.0615.totu@gmail.com>
2020-05-11 15:26:26 +0900

    crypto モジュールをHTML上に表示

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,10 @@
 'use strict';
 import dc from 'damage-calc';
+import crypto from 'crypto';
+
 const root = document.getElementById('root');
 root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
   + dc.effectiveDamage(100, 50, 30) + '</p>';
+
+const root2 = document.getElementById('root2');
+root2.innerHTML = '<p>' + crypto.randomBytes(8).toString('hex') + '</p>';
