d49cdca3a358f7a2a6ec7ff06937fe8611821385
Makino Yudi <hymakino@icloud.com>
2020-10-12 23:37:49 +0900

    Node.jsのcryptoモジュールをクライアント側に表示できるように実装した

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,9 @@
 'use strict';
-import dc from 'damage-calc';
+// import dc from 'damage-calc';
+// const root = document.getElementById('root');
+// root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
+//   + dc.effectiveDamage(100, 50, 30) + '</p>';
+
+import crypto from 'crypto';
 const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+root.innerHTML = '<p>' + crypto.randomBytes(8).toString('hex') + '</p>';
