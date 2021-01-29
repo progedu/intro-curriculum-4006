27c812fddbd99a90fd195c6d56c5f54c4aaddd32
chikako2020 <smkdualclean-3@yahoo.co.jp>
2021-01-29 19:53:31 +0900

    crypto モジュールを動かすように改変

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
-import dc from 'damage-calc';
+const dc = require ('damage-calc');
+const crypto = require ('crypto');
 const root = document.getElementById('root');
 root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+  + dc.effectiveDamage(100, 50, 30) + 
+  '</p>今日のあなたのラッキー文字列は、 ' 
+  + crypto.randomBytes(8).toString('hex') + '</p>';
