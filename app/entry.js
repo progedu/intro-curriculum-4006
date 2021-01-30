d0c6c1f371055c9793e2417793ab1d02b28eca12
yos10 <yos.ohtake+git@gmail.com>
2021-01-30 16:31:13 +0900

    ランダムな 8 バイトの値の 16 進数の文字列を、 HTML 上に表示

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,11 @@
 'use strict';
 import dc from 'damage-calc';
+import crypto from 'crypto';
 const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+root.innerHTML =
+  '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、' +
+  dc.effectiveDamage(100, 50, 30) +
+  '</p>';
+const p = document.createElement('p');
+p.textContent = crypto.randomBytes(8).toString('hex');
+root.appendChild(p);
