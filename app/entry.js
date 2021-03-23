1f4e8f54f6f669b9b7f6fe4f665451a9cdbe1248
Aoi Yamaguchi <yamaguchiaoi0130@gmail.com>
2021-03-23 19:46:42 +0900

    6

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,10 @@
 'use strict';
 import dc from 'damage-calc';
-const root = document.getElementById('root');
-root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
-  + dc.effectiveDamage(100, 50, 30) + '</p>';
+// const root = document.getElementById('root');
+// root.innerHTML = '<p>攻撃力 100, 防御 50, 防御貫通 30 のダメージは、'
+//   + dc.effectiveDamage(100, 50, 30) + '</p>';
+
+  //追加
+  import crypto from 'crypto';
+  const root = document.getElementById('root');
+  root.innerHTML = '<p>' + crypto.randomBytes(8).toString('hex') + '</p>';
